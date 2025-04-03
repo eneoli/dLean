7378a0c2e8af0386a7d58f1dbe2e555c567444ca
Oliver Enes <oliver@eneoli.de>
2025-04-04 00:53:45 +0200

    WIP Differential

--- DLean/Semantics.lean
+++ DLean/Semantics.lean
@@ -1,8 +1,15 @@
+import Mathlib.Analysis.Calculus.Deriv.Basic
+
 import DLean.Syntax
 import DLean.Interpretation
 import DLean.State
 
-def dynamicSemantics (i: Interpretation) (s : State) (t : Term) : ℝ :=
+example {f : ℝ → ℝ} {x a : ℝ} (h : HasDerivAt f a x) : deriv f x = a :=
+  h.deriv
+
+#check HMul
+
+noncomputable def Term.dynamicSemantics (i: Interpretation) (s : State) (t : Term) : ℝ :=
   match t with
     | Term.var  v         => s v
     | Term.neg  t         => - dynamicSemantics i s t
@@ -10,7 +17,13 @@ def dynamicSemantics (i: Interpretation) (s : State) (t : Term) : ℝ :=
     | Term.times x y      => dynamicSemantics i s x * dynamicSemantics i s y
     | Term.applyFn f args => let argValues := .map (fun ⟨e, h⟩ => dynamicSemantics i s e) args.toVector.attach
                              i f argValues
-    | Term.differential t => sorry
+    | Term.differential t => let fvars := t.freeAssignables
+                             List.sum $ fvars.map (
+                              fun x =>
+                                s (Assignable.diff x) *
+                                (deriv (fun y => dynamicSemantics i (fun x' => if x = x' then y else s x)) (s x)) t
+                                -- (deriv (fun x => dynamicSemantics i (fun v => if true then 1 else s v) t) (s x'))
+                             )
 decreasing_by
   all_goals try decreasing_trivial
   have t : e ∈ args := (TermVector.mem_toVector_iff e args).mpr h
