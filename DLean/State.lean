8f170fc03ac335e302b5fac22cf37c1029ddcee7
Oliver Enes <oliver@eneoli.de>
2025-04-09 22:44:26 +0200

    added State.isEq and State.isEqExcept

--- DLean/State.lean
+++ DLean/State.lean
@@ -6,6 +6,11 @@ def State.update (s : State) (assignable : Assignable) (r : ℝ) : State := fun 
     if a = assignable then r
     else s a
 
+def State.isEq (s₁ : State) (s₂ : State) : Prop :=
+    ∀x:Assignable, s₁ x = s₂ x
+
+def State.isEqExcept (s₁ : State) (s₂ : State) (except : List Assignable) :=
+    ∀x:Assignable, ¬x ∈ except → s₁ x = s₂ x
 
 lemma UpdatedStateIsIndeedUpdated (s : State) (a : Assignable) (r : ℝ) :
     s.update a r a = r := by
