cc6514a1e09633a235236122f0a44374fb7dcee3
Oliver Enes <oliver@eneoli.de>
2025-05-06 22:17:32 +0200

    added bouncing ball example

---
+++ DLean/Examples/BouncingBall.lean
@@ -0,0 +1,7 @@
+import DLean.Syntax
+import DLean.Embedding.Embedding
+
+theorem bouncing_ball : Provable (⌈⊢ 0.0 ≤ x ∧ x = H ∧ v = 0 ∧ g > 0 ∧ 1 ≥ c ∧ c ≥ 0 →
+[(x' = v, v' = -g & x ≥ 0; (?x = 0; v := -c*v ∪ ?x ≠ 0))*] (0 ≤ x ∧ x ≤ H)⌉)
+  := by
+  sorry
