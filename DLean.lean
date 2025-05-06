bb1e331952e7cf23db775d6e9afc84871b94670e
Oliver Enes <oliver@eneoli.de>
2025-05-06 22:15:45 +0200

    added syntax and semantics

--- DLean.lean
+++ DLean.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `DLean` library.
 -- Import modules here that should be built as part of the library.
-import DLean.Basic
+
+import DLean.Syntax
+import DLean.Semantics.Semantics
