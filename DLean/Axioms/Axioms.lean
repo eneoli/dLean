fedf23241f788771886c2b69363101bce4b1354f
Oliver Enes <oliver@eneoli.de>
2026-06-06 03:16:29 +0200

    feat(Axioms): added some axioms

--- DLean/Axioms/Axioms.lean
+++ DLean/Axioms/Axioms.lean
@@ -6,26 +6,10 @@ import DLean.Semantics.Interpretation
 import DLean.Semantics.DynamicSemantics
 
 import DLean.Axioms.Base
+import DLean.Axioms.DC
+import DLean.Axioms.DE
 import DLean.Axioms.DG
-import DLean.Axioms.Refinement
 import DLean.Axioms.DiffTerm
 import DLean.Axioms.DL
-
-open Semantics
-open Embedding
-
-theorem dW : ⌈[x’ = f(x) & q(x)]q(x)⌉ := by
-  intros i s
-  simp[Formula.denote, setOf]
-  unfold Program.denote
-  simp
-  simp[odeEvolutionFormula, Formula.denote]
-  simp[Membership.mem, Set.Mem]
-  intros w r Hr φ Hstart Hend Hinv
-  have ⟨⟨left,H⟩, right⟩ := Hinv r ?goal (by simp)
-  . clear left right
-    revert H
-    simp[TermVector.toVector]
-    simp[Term.denote]
-    simp[Hend]
-  . trivial
+import DLean.Axioms.DW
+import DLean.Axioms.Refinement
